d74054b94ad4447fde2c1dece28c1e0f65e29715
1lymax <59887205+1lymax@users.noreply.github.com>
2022-12-28 17:37:17 +0200

    file service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import {Module} from "@nestjs/common";
 import {TrackModule} from "./track/track.module";
 import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
 
 @Module( {
     imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static'),}),
         MongooseModule.forRoot('mongodb+srv://musicplatform:o7P25vBJKUYT2W0i@cluster0.5etmffb.mongodb.net/?retryWrites=true&w=majority'),
-        TrackModule
+        TrackModule,
+        FileModule,
     ]
 })
 export class AppModule {}
