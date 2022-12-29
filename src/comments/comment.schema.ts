5f87d8193a4d5e6462e31ea04e7f1a88a688d6ff
1lymax <59887205+1lymax@users.noreply.github.com>
2022-12-29 14:25:32 +0200

    comments

--- src/comments/comment.schema.ts
+++ src/comments/comment.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import {Track} from "./track.schema";
+import {Track} from "../track/track.schema";
 import * as mongoose from "mongoose";
 
 export type CommentDocument = HydratedDocument<Comment>;
