08d0ccc40e90cfba6a769c32d5cb1d5b3418f1e4
1lymax <59887205+1lymax@users.noreply.github.com>
2022-12-29 14:40:46 +0200

    fix types

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,8 +1,8 @@
 import {ObjectId} from "mongoose";
 
 export class CreateTrackDto {
-    readonly name;
-    readonly text;
+    readonly name: string;
+    readonly text: string;
     readonly artistId: ObjectId;
     readonly albumId: ObjectId;
 }
