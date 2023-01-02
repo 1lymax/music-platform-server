9e828c55924e7c4b6fa6a298b4b16618904a22a8
1lymax <59887205+1lymax@users.noreply.github.com>
2023-01-02 15:08:31 +0200

    fix populate album and artist

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -44,6 +44,8 @@ export class TrackService {
 
     async getAll(count = 10, offset = 0): Promise<Track[]> {
         const tracks = await this.trackModel.find().skip(Number(offset)).limit(Number(count))
+            .populate('artistId')
+            .populate('albumId')
         return tracks
     }
 
